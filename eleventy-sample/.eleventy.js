70ad345e282913a2a66130c0bf30145b1f5e2542
tjay061 <tjay063@gmail.com>
2024-01-22 14:24:20 -0500

    idk  bro

--- eleventy-sample/.eleventy.js
+++ eleventy-sample/.eleventy.js
@@ -1,4 +1,4 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("style.css");
+    eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("images");
   };
